dea3b5375db715b54150a5cf4b59986207ffcdf6
AkakiyBerula <vlad5dyakun.gmail.com>
2023-11-02 19:29:24 +0200

    Labs 5

---
+++ Lab5/lab5.py
@@ -0,0 +1,30 @@
+def mul02(byte):
+    if byte & 0x80:
+        result = (byte << 1) ^ 0x1B
+    else:
+        result = byte << 1
+    return result & 0xFF
+
+
+def mul03(byte):
+    return mul02(byte) ^ byte
+
+
+byte1 = 0xD4
+byte2 = 0xBF
+
+
+result1 = mul02(byte1)
+result2 = mul03(byte2)
+
+print(byte1)
+print(byte2)
+
+
+print(hex(result1))
+print(hex(result2))
+
+print(result1)
+print(result2)
+
+
